51d354a56085838006703b803ade82a7fa2cd0b8
ButorinNikita <104126800+ButorinNikita@users.noreply.github.com>
2022-04-21 15:02:48 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,411 @@
+//Обязательные предметы 
+arrayEGE = new Array()
+arrayObjects = new Array()
+
+
+arrayObjects[0] = {
+    name: 'Экология и природопользование', //Наименование
+    profile: 'Экология', //Профиль
+    key1: [0, 1, 6], //Ключ по которому можно сверить предмет
+    key2: [0, 4, 6],
+    key3: [0, 6, 7],
+    key4: [0, 6, 8],
+    key5: []
+}
+
+arrayObjects[1] = {
+    name: 'Педагогическое образование',
+    profile: 'Право',
+    key1: [0, 1, 3],
+    key2: [0, 3, 5],
+    key3: [0, 3, 9],
+    key4: [],
+    key5: []
+
+}
+
+arrayObjects[2] = {
+    name: 'Педагогическое образование',
+    profile: 'История',
+    key1: [0, 1, 3],
+    key2: [0, 3, 5],
+    key3: [0, 3, 9],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[3] = {
+    name: 'Педагогическое образование',
+    profile: 'Дошкольное образование',
+    key1: [0, 1, 3],
+    key2: [0, 3, 6],
+    key3: [],
+    key4: [],
+    key5: [],
+
+}
+
+arrayObjects[4] = {
+    name: 'Психолого-педагогическое образование',
+    profile: 'Психология образования',
+    key1: [0, 1, 6],
+    key2: [0, 3, 6],
+    key3: [0, 6, 9],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[5] = {
+    name: 'Юриспруденция',
+    profile: 'Юриспруденция',
+    key1: [0, 3, 5],
+    key2: [0, 3, 9],
+    key3: [],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[6] = {
+    name: 'Строительство',
+    profile: 'Промышленное и гражданское строительство',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [0, 1, 9],
+    key5: []
+}
+
+arrayObjects[7] = {
+    name: 'Строительство',
+    profile: 'Экспертиза и управление недвижимостью',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [0, 1, 9],
+    key5: []
+}
+
+arrayObjects[8] = {
+    name: 'Строительство',
+    profile: 'Информационно-строительный инжиниринг',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [0, 1, 9],
+    key5: []
+}
+
+arrayObjects[9] = {
+    name: 'Прикладная информатика',
+    profile: 'Прикладная информатика в экономике',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [0, 1, 9],
+    key5: []
+}
+
+arrayObjects[10] = {
+    name: 'Экономика',
+    profile: 'Финансы и кредит',
+    key1: [0, 1, 3],
+    key2: [0, 1, 4],
+    key3: [0, 1, 5],
+    key4: [0, 1, 7],
+    key5: [0, 1, 9]
+}
+
+arrayObjects[11] = {
+    name: 'Менеджмент',
+    profile: 'Государственное и муниципальное управление',
+    key1: [0, 1, 3],
+    key2: [0, 1, 4],
+    key3: [0, 1, 5],
+    key4: [0, 1, 7],
+    key5: [0, 1, 9]
+}
+
+arrayObjects[12] = {
+    name: 'Управление персоналом',
+    profile: 'Управление персоналом организации',
+    key1: [0, 1, 3],
+    key2: [0, 1, 4],
+    key3: [0, 1, 5],
+    key4: [0, 1, 7],
+    key5: [0, 1, 9]
+}
+
+arrayObjects[13] = {
+    name: 'Прикладная математика и информатика',
+    profile: 'Инженерия программного обеспечения',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[14] = {
+    name: 'Информационные системы и технологии',
+    profile: 'Информационные системы и технологии',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[15] = {
+    name: 'нфокоммуникационные технологии и системы связи',
+    profile: 'Многоканальные телекоммуникационные системы',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[16] = {
+    name: 'Теплоэнергетика и теплотехника',
+    profile: 'Промышленная теплоэнергетика',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[17] = {
+    name: 'Электроэнергетика и электротехника',
+    profile: 'Электроэнергетика',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[18] = {
+    name: 'Управление в технических системах',
+    profile: 'Управление и информатика  в технических системах',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[19] = {
+    name: 'Технологические машины и оборудование',
+    profile: 'Промышленная теплоэнергетика',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[20] = {
+    name: 'Электроэнергетика и электротехника',
+    profile: 'Электроэнергетика',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[21] = {
+    name: 'Управление в технических системах',
+    profile: 'Управление и информатика  в технических системах',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[22] = {
+    name: 'Управление в технических системах',
+    profile: 'Управление и информатика  в технических системах',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[23] = {
+    name: 'Технологические машины и оборудование',
+    profile: 'Машины и оборудование лесного комплекса',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[24] = {
+    name: 'Конструкторско-технологическое обеспечение машиностроительных производств',
+    profile: 'Технология машиностроения',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[25] = {
+    name: 'Наземные транспортно-технологические комплексы',
+    profile: 'Подъемно-транспортные строительные дорожные машины и оборудование',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[26] = {
+    name: 'Эксплуатация транспортно-технологических машин и комплексов',
+    profile: 'Автомобили и автомобильное хозяйство',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 8],
+    key4: [],
+    key5: []
+}
+
+arrayObjects[27] = {
+    name: 'Лесное дело',
+    profile: 'Лесное дело',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 6],
+    key4: [0, 1, 7],
+    key5: [0, 1, 8]
+}
+
+arrayObjects[28] = {
+    name: 'Технология лесозаготовительных и деревоперерабатывающих производств',
+    profile: 'Управление качеством в лесозаготовительном производстве',
+    key1: [0, 1, 2],
+    key2: [0, 1, 4],
+    key3: [0, 1, 6],
+    key4: [0, 1, 8],
+    key5: []
+}
+
+arrayObjects[29] = {
+    name: 'Технология лесозаготовительных и деревоперерабатывающих производств',
+    profile: 'Лесоинженерное дело',
+    key1: [0, 1, 2],
+    key2: [0, 1, 6],
+    key3: [0, 1, 7],
+    key4: [0, 1, 8],
+    key5: []
+}
+
+arrayObjects[30] = {
+    name: 'Технология лесозаготовительных и деревоперерабатывающих производств',
+    profile: 'Технология деревообработки',
+    key1: [0, 1, 2],
+    key2: [0, 1, 6],
+    key3: [0, 1, 7],
+    key4: [0, 1, 8],
+    key5: []
+}
+
+arrayObjects[31] = {
+        name: 'Ландшафтная архитектура',
+        profile: 'Садово-парковое и ландшафтное строительство',
+        key1: [0, 1, 2],
+        key2: [0, 1, 6],
+        key3: [0, 1, 7],
+        key4: [0, 1, 8],
+        key5: []
+    }
+    //Основная функция
+function asd() {
+    console.log('GOOD!')
+
+    loadEGE()
+    console.log(arrayEGE)
+    for (let i = 0; i < arrayObjects.length; i++) {
+        let count = 0
+        for (let j = 0; j < 3; j++) {
+            if (arrayEGE[j] == arrayObjects[i].key1[j]) {
+                count++
+            }
+            if (count == 3) {
+                document.writeln('Направление:  ' + arrayObjects[i].name + 'Специальность :  ' + arrayObjects[i].profile + '<br>')
+                count = 0
+            }
+        }
+        count = 0
+        for (let j = 0; j < 3; j++) {
+            if (arrayEGE[j] == arrayObjects[i].key2[j]) {
+                count++
+            }
+            if (count == 3) {
+                document.writeln('Направление:  ' + arrayObjects[i].name + 'Специальность :  ' + arrayObjects[i].profile + '<br>')
+                count = 0
+            }
+        }
+        count = 0
+        for (let j = 0; j < 3; j++) {
+            if (arrayEGE[j] == arrayObjects[i].key3[j]) {
+                count++
+            }
+            if (count == 3) {
+                document.writeln('Направление:  ' + arrayObjects[i].name + 'Специальность :  ' + arrayObjects[i].profile + '<br>')
+                count = 0
+            }
+        }
+        count = 0
+        for (let j = 0; j < 3; j++) {
+            if (arrayEGE[j] == arrayObjects[i].key4[j]) {
+                count++
+            }
+            if (count == 3) {
+                document.writeln('Направление:  ' + arrayObjects[i].name + 'Специальность :  ' + arrayObjects[i].profile + '<br>')
+                count = 0
+            }
+        }
+        count = 0
+        for (let j = 0; j < 3; j++) {
+            if (arrayEGE[j] == arrayObjects[i].key5[j]) {
+                count++
+            }
+            if (count == 3) {
+                document.writeln('Направление:  ' + arrayObjects[i].name + 'Специальность :  ' + arrayObjects[i].profile + '<br>')
+                count = 0
+            }
+        }
+    }
+}
+
+//Введенные данные переводим в массив
+function loadEGE() {
+    arrayEGE = [];
+    if (document.getElementById("russ").value >= 40)
+        arrayEGE.push(0)
+    if (document.getElementById("math").value >= 39)
+        arrayEGE.push(1)
+    if (document.getElementById("phy").value >= 44)
+        arrayEGE.push(2)
+    if (document.getElementById("obsh").value >= 42)
+        arrayEGE.push(3)
+    if (document.getElementById("ikt").value >= 40)
+        arrayEGE.push(4)
+    if (document.getElementById("hist").value >= 40)
+        arrayEGE.push(5)
+    if (document.getElementById("bio").value >= 40)
+        arrayEGE.push(6)
+    if (document.getElementById("heo").value >= 40)
+        arrayEGE.push(7)
+    if (document.getElementById("chem").value >= 40)
+        arrayEGE.push(8)
+    if (document.getElementById("eng").value >= 40)
+        arrayEGE.push(9)
+    return arrayEGE
+}
